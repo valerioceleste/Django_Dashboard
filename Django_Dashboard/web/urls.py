71b10939d059b499b2bd60eed057e757554c32c4
Valerio Celeste <valerioceleste@192.168.1.2>
2023-12-08 17:41:20 +0100

    Initialized Apps

---
+++ Django_Dashboard/web/urls.py
@@ -0,0 +1,10 @@
+
+from django.contrib import admin
+from django.urls import include, path
+from . import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+	path('', views.home, name='home'),
+	path('dash/', include('dash.urls')),
+]
