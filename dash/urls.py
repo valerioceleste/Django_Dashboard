14e71b487468bf874466e1456d8ff91a734c05f0
Valerio Celeste <valerioceleste@192.168.1.2>
2023-12-08 23:18:11 +0100

    Addedd AUTH modules

--- dash/urls.py
+++ dash/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
 
     path('', views.index, name='index'),
+    path('registration/', views.registration, name='registration'),
+    path('login/', views.login, name='login'),
 
 ]
