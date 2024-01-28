6876a6e50fde0d87d3f377781474529814507d25
Valerio Celeste <valerioceleste@192.168.1.54>
2024-01-28 16:24:56 +0100

    Solved AUTH issue and added alerts

--- dash/views.py
+++ dash/views.py
@@ -5,45 +5,49 @@ from django.contrib.auth.models import User
 from django.contrib import messages
 import logging
 
+logger = logging.getLogger(__name__)
+
 def index(request):
     return render(request, 'dash/index.html')
 
-def user_login(request): #aggiungere warning utoente o password non correta
-    context = {}
+def user_login(request):
+
     if request.method == "POST":
         username = request.POST['username']
         password = request.POST['psw']
         user = authenticate(username=username, password=password)
+
         if user is not None:
             login(request, user)
             return redirect('dash:index')
         else:
-            return render(request, 'dash/login.html', context)
-    else:
-            return render(request, 'dash/login.html', context)
-    
-from django.contrib.auth.models import User
-from django.contrib.auth import login
-from django.shortcuts import render, redirect
+            messages.error(request, 'Incorrect username or password. Please try again.')
 
+    return render(request, 'dash/login.html')
+    
 def registration_request(request):
     context = {}
+
     if request.method == 'GET':
         return render(request, 'dash/signup.html', context)
+    
     elif request.method == 'POST':
         username = request.POST['username']
         password = request.POST['psw']
+        confirmpsw = request.POST['confirmpsw']
         email = request.POST['email']
-        user_exist = False
-        try:
-            User.objects.get(username=username)
-            user_exist = True
-        except User.DoesNotExist:
-            logger.debug("{} is a new user".format(username))
-        if not user_exist:
-            user = User.objects.create_user(username=username, email=email, password=password)
-            login(request, user)
-            return redirect("dash:index")
-        else:
+        
+        if password != confirmpsw:
+            messages.error(request, 'Passwords do not match. Please enter matching passwords.')
             return render(request, 'dash/signup.html', context)
 
+        try:
+            existing_user = User.objects.get(username=username)
+            messages.error(request, 'User already exists. Please choose a different username.')
+            return render(request, 'dash/signup.html', context)
+        except User.DoesNotExist:
+            user = User.objects.create_user(username=username, email=email, password=password)
+            login(request, user)
+            messages.success(request, 'New user created with success.')
+            return redirect("dash:index")
+            
